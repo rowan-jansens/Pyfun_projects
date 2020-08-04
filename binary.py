8663f625c05eec36ce8245727e7eb06eb70e7a50
Rowan Jansens <rowan.jansens@gmail.com>
2020-08-04 14:13:16 -0600

    Annotated binary

--- binary.py
+++ binary.py
@@ -1,43 +1,51 @@
+"""A simple program to convert a number into binary
+Coded By: Rowan Jansens"""
+
 import math
 
 def main():
-    num = int(input("Enter a number: \n"))
-    print(binary_convert(num))
+    num = int(input("Enter a number: \n"))   #get number to be converted
+    binary_convert(num)
 
 def binary_convert(num): 
-    b = {}
-    j = int(math.log(get_gf(num)) / math.log(2))
+    b = []     #make an empty list
+    #determine the number of binary factors (2^n) needed to express the input number
+    num_factors = int(math.log(get_bf(num)) / math.log(2))   
 
-    #make empty dict with the right number of factors
-    for i in range(j, -1, -1):
-        b[(2 ** i)] = b.get((2 ** i), 0)
+    #append list with lists of the binary factors calucated above and a value of 0
+    for i in range(num_factors, -1, -1):
+        b.append([2**i, 0])
+    
+    for i in range(len(b)):   #loop through the list
+        #If the num is larger then the binary factor for current position, change state to 1
+        if (num >= b[i][0]):
+            b[i][1] = 1
+            num -= b[i][0]   #Then update the num by subtracting the factor from it
 
-    #if a number has gf as a facotr, add 1 to the dict for that factor
-    #then decrase the number by that much
-    for i in range(len(b)):
-        gf = get_gf(num)
-        if (gf <= num):
-            b[get_gf(num)] = b.get(get_gf(num)) + 1
-            num = num - gf
-    print(b)
-    binary = make_str(b)
-    return binary
+    make_dict(b)  #print list as dict
+    make_str(b)   #print a string of the list values
 
- #find the largest factor of the order 2^n
-def get_gf(num):
-    gf = 1
+#find the largest binary factor (of the order 2^n) of the current number
+def get_bf(num):
+    binary_factor = 1  #smallest factor is always 1
     n = 0
-    while (num >= (gf + (2 ** n))):
-        gf += (2 ** n)
+    while (num >= (binary_factor + (2 ** n))):    #increment binary factor by the next factor (2^n) as long as num is larger
+        binary_factor += (2 ** n)
         n += 1
-    return gf
+    return binary_factor
 
-#print the dict as a string from left to right
+#print the list as a dictionary
+def make_dict(b):
+    d = {}
+    for i in range(len(b)):
+        d.update({int(b[i][0]):int(b[i][1])})
+    print(d)
+
+#print the list values as a string
 def make_str(b):
-    string = ""
-    length = len(b) - 1
-    for i in range(length, -1, -1):
-        string += str(b[2 ** i])
-    return string
+    s = ""
+    for i in range(len(b)):
+        s += str(b[i][1])
+    print(s)
      
 main()
