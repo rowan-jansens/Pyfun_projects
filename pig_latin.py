5e777fc7dec0f156a913eab23440afbd31aa2221
Rowan Jansens <rowan.jansens@gmail.com>
2020-08-04 12:30:20 -0600

    Annotated pig_latin

--- pig_latin.py
+++ pig_latin.py
@@ -7,27 +7,29 @@ def main():
     pig_latin(phrase)
 
 def pig_latin(phrase):
-    words = phrase.split()     #break the phrase up into single  words
-    wc = len(words)
-    for i in range(wc):     #iterate through the all the words
+    new_phrase = []
+    words = phrase.split()     #break the phrase up into a list of single  words
+    for i in range(len(words)):     #iterate through all the words in the list
         vowel = first_vow(words[i])     # get the position of first vowel
-        #make a the new words by cutting off the first bit and adding a sufix
-        new_word = str(words[i][vowel:] + sufix(words[i], vowel))
-        #print the new words
-        print(str(new_word))
         
-def sufix(words, vowel):
-    #make sufix by adding "ay" to the letter before the fist vowel
+        #make new word by cutting off everything before the fist vowel and adding a suffix
+        new_word = str(words[i][vowel:] + suffix(words[i], vowel))
+        new_phrase.append(new_word)   #add new word to the "new_phrase" list
+    print(' '.join(w for w in new_phrase))   #print the words in the list like a sentence
+        
+def suffix(words, vowel):
+    #make suffix by adding "ay" to the letters before the fist vowel
     s = str('-' + words[0:vowel] + 'ay')
     return s   
     
 def first_vow(words):
-    length = len(words)     #legth of an individual word
     vow = ['a', 'e', 'i', 'o', 'u', 'y']
-    #iterate through the letters untill a vowel is reached
-    #then return its possition in the word
-    for j in range(length):
-        for k in range (6):
+    
+    #iterate through the letters of the given word
+    for j in range(len(words)):
+        #try to match a vowel in the word
+        for k in range (len(vow)):
+            #if matched, return current position
             if (words[j] == vow[k]):
                 return j
 main()
