3226fdd3e2dc28e823463c98160be0e644c3e172
Rowan Jansens <rowan.jansens@gmail.com>
2020-08-04 12:52:27 -0600

    Annotated integrate

--- integrate.py
+++ integrate.py
@@ -5,21 +5,27 @@ Coded by: Rowan Jansens"""
 import math
 
 def main():
-    input_f = input('Enter a function (f(x)): ')   #get input function
+    input_f = input('Enter a python interpretable function (f(x)): ')   #get input function
     lower = int(input('Enter lower bound: '))     #get integral bounds
     upper = int(input('Enter upper bound: '))
-    integrate(10000, lower, upper, input_f)
+    if (lower > upper):  #ensures proper calucation of "backward" integrals
+        integrate(upper, lower, input_f, True)   #switch bounds and set "backward" to true
+    else:
+        integrate(lower, upper, input_f, False)  #or just integrate normally
     
-def integrate(dx, lower, upper, input_f):
+def integrate(lower, upper, input_f, backward):
     area = 0
+    dx = 1000   #num sub devisions --> increases accuracy
     for i in range(lower, upper):     #iterate through the bounds...
         for j in range(dx):    #...and sub divisions 
             val = ((2 * i) + (j / dx) + ((j + 1) / dx)) / 2  #compute trapezoidal height
-            area += (function(val, input_f) * (1 / dx))  #compute trapezoidal area using the functiona and trapezoid height
+            area += (function(val, input_f) * (1 / dx))  #compute trapezoidal area
+    if (backward):
+        area *= -1   #switch the sign of backward integrals
     print(area)
 
 def function(x, input_f):
-    y = eval(input_f)   #evaluate the area using function and heigh
+    y = eval(input_f)   #evaluate the input function at given height
     return y
 
 main()
